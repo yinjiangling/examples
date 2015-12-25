655d07ebd17d911670381b17fd60ec3a5dc742ea
yinjiangling <yjlumac@gmail.com>
2015-12-25 00:35:44 -0500

    Create Hbase Table

---
+++ Hbase_examples/CreateTable.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.TableName;
+
+import org.apache.hadoop.conf.Configuration;
+
+public class CreateTable {
+      
+   public static void main(String[] args) throws IOException {
+	Configuration con = HBaseConfiguration.create();
+	HBaseAdmin admin = new HBaseAdmin(con);
+	HTableDescriptor tableDescriptor = new HTableDescriptor(TableName.valueOf("emp"));
+	tableDescriptor.addFamily(new HColumnDescriptor("personal"));
+	tableDescriptor.addFamily(new HColumnDescriptor("professional"));
+	admin.createTable(tableDescriptor);
+	System.out.println("create table: emp");
+ }
+}
+	
+
---
+++ script_examples/java.sh
@@ -0,0 +1,5 @@
+export JAVA_HOME=/home/ji453898/jiangling/Java/jdk1.7.0_79
+export JRE_HOME=$JAVA_HOME/jre
+export PATH=$JAVA_HOME/bin:$PATH
+export CLASSPATH=$JAVA_HOME/lib:$JAVA_HOME/lib/tools.jar:$CLASSPATH
+export LD_LIBRARY_PATH=$JRE_HOME/lib/amd64/server:$LD_LIBRAEY_PATH
